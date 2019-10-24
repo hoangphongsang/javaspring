4c4ebf017c8ec638395fa375403c299bad25b5e9
SangHP <SangHP@DESKTOP-Q7RSS6D>
2019-10-24 20:43:45 +0700

    test

---
+++ MiniProject/src/main/java/com/kin/entity/chucvu.java
@@ -0,0 +1,31 @@
+package com.kin.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "chucvu")
+public class chucvu {
+	@Id
+	int id;
+	String tenchucvu;
+	String ghichu;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTenchucvu() {
+		return tenchucvu;
+	}
+	public void setTenchucvu(String tenchucvu) {
+		this.tenchucvu = tenchucvu;
+	}
+	public String getGhichu() {
+		return ghichu;
+	}
+	public void setGhichu(String ghichu) {
+		this.ghichu = ghichu;
+	}
+	
+}
